0fec7391f7cc436fdd78ae8c5e1d8a6e6c475b64
mtt-office <mtt.5ffice@gmail.com>
2025-08-23 15:29:57 +0900

    커밋 메시지

--- js/script.js
+++ js/script.js
@@ -14,88 +14,81 @@ Runner.run(runner, engine);
 function createProjectBoxes() {
   const projects = [
     {
-      width: 200,
-      height: 130,
-      imgSrc: 'assets/images/projects/mtt.jpeg',
-      link: 'https://www.instagram.com/mtt.office/'
-    },
-    {
-      width: 90,
-      height: 125,
-      imgSrc: 'assets/images/projects/tooth.jpeg',
-      link: 'https://www.instagram.com/p/DLnnO8kzOB3/?img_index=1'
-    },
-    {
-      width: 90,
-      height: 130,
-      imgSrc: 'assets/images/projects/book.jpeg',
-      link: 'https://www.instagram.com/p/DLnnO8kzOB3/?img_index=1'
-    },
-    {
-      width: 160,
-      height: 100,
-      imgSrc: 'assets/images/projects/ball.jpeg',
-      link: 'https://www.instagram.com/p/DLnnO8kzOB3/?img_index=1'
-    },
-    {
-      width: 140,
-      height: 170,
-      imgSrc: 'assets/images/projects/ny.jpeg',
-      link: 'https://www.shopiloveny.com/?srsltid=AfmBOopRTW3Qw_O0tD4hOG3QmVYPmeGrzWH2W6ZRE2ql363iBtykRmKb'
-    },
-    {
-      width: 160,
-      height: 160,
-      imgSrc: 'assets/images/projects/rainbowcircle.jpeg',
-      link: 'https://en.wikipedia.org/wiki/Damien_Hirst'
-    },
-    {
-      width: 90,
-      height: 90,
-      imgSrc: 'assets/images/projects/jeju.jpeg',
-      link: 'https://www.kma.go.kr/jeju/html/main/index.jsp'
+      width: 180,
+      height: 120,
+      imgSrc: 'assets/images/projects/mtt.png',
+
     },
     {
       width: 180,
-      height: 100,
-      imgSrc: 'assets/images/projects/corn.jpeg',
-      link: 'https://en.wikipedia.org/wiki/Green'
+      height: 120,
+      imgSrc: 'assets/images/projects/tooth.png',
     },
     {
-      width: 80,
-      height: 80,
-      imgSrc: 'assets/images/projects/chair.jpeg',
-      link: 'https://en.wikipedia.org/wiki/History_of_the_chair'
+      width: 180,
+      height: 120,
+      imgSrc: 'assets/images/projects/book.png',
     },
     {
-      width: 90,
-      height: 150,
-      imgSrc: 'assets/images/projects/sound.jpeg',
-      link: 'https://www.instagram.com/p/DLnn4PazFZ9/?img_index=1'
+      width: 180,
+      height: 120,
+      imgSrc: 'assets/images/projects/ball.png',
     },
     {
-      width: 93,
-      height: 130,
-      imgSrc: 'assets/images/projects/ant.jpeg',
-      link: 'https://www.instagram.com/p/DEkfGYkvNiM/?img_index=1'
+      width: 180,
+      height: 120,
+      imgSrc: 'assets/images/projects/ny.png',
     },
     {
-      width: 60,
+      width: 180,
+      height: 120,
+      imgSrc: 'assets/images/projects/rainbowcircle.png',
+    },
+    {
+      width: 180,
+      height: 120,
+      imgSrc: 'assets/images/projects/jeju.png',
+    },
+    {
+      width: 180,
+      height: 120,
+      imgSrc: 'assets/images/projects/corn.png',
+    },
+    {
+      width: 180,
+      height: 120,
+      imgSrc: 'assets/images/projects/chair.png',
+    },
+    {
+      width: 180,
+      height: 120,
+      imgSrc: 'assets/images/projects/sound.png',
+    },
+    {
+      width: 180,
+      height: 120,
+      imgSrc: 'assets/images/projects/ant.png',
+    },
+
+    {
+      width: 180,
+      height: 120,
+      imgSrc: 'assets/images/projects/d.png',
+    },
+    {
+      width: 180,
+      height: 120,
+      imgSrc: 'assets/images/projects/a.png',
+    },
+    {
+      width: 180,
+      height: 120,
+      imgSrc: 'assets/images/projects/o.png',
+    },
+    {
+      width: 180,
       height: 160,
-      imgSrc: 'assets/images/projects/zig.jpeg',
-      link: 'https://ddp.or.kr/?menuno=228'
-    },
-    {
-      width: 120,
-      height: 140,
-      imgSrc: 'assets/images/projects/choii.jpeg',
-      link: 'https://www.instagram.com/p/DLQ2NbjzoCA/?img_index=1'
-    },
-    {
-      width: 120,
-      height: 140,
-      imgSrc: 'assets/images/projects/spring.jpeg',
-      link: 'https://namu.wiki/w/%ED%9B%84%ED%81%AC%20%EB%B2%95%EC%B9%99'
+      imgSrc: 'assets/images/projects/p.png',
     }
   ];
 
